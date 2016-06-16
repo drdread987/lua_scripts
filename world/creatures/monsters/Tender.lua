7fee22fc8d814f8b3414e7a86c02d61061a412c8
Revoltorn <revoltorn@gmail.com>
2016-06-15 17:36:23 -0700

    A tender test

---
+++ world/creatures/monsters/Tender.lua
@@ -0,0 +1,18 @@
+local npcID = 15271
+local aggro_spell = 11922
+
+function OnEnterCombat (event, creature, target}
+
+    creature:CastSpell(target, aggro_spell, false)
+    
+end
+
+
+function OnEnterCombat (event, creature)
+
+    creature:SendUnitSay("Deforestation is not the answer!", 0)
+    
+end
+
+
+RegisterCreatureEvent(npcID, 1, OnEnterCombat)
