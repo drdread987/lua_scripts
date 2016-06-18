777709e099301866d8e1379776e2ed3b3f383305
Revoltorn <revoltorn@gmail.com>
2016-06-17 19:24:30 -0700

    Tender Fixes

--- world/creatures/monsters/Feral_tender.lua
+++ world/creatures/monsters/Feral_tender.lua
@@ -1,15 +1,14 @@
-local npcID = 15271
+local npcID = 15294
 local aggro_spell = 339
 
 function OnEnterCombat (event, creature, target)
 
     creature:CastSpell(target, aggro_spell, false)
 
-    creature:SendUnitSay("Deforestation is not the answer!", 0)
+    creature:SendUnitYell("Stay out of the forest heathens!", 0)
     
 end
 
 
 RegisterCreatureEvent(npcID, 1, OnEnterCombat)
 
--- For Testing
